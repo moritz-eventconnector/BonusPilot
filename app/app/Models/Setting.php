e7cd77ef54b35b4e9f37681660870eefbc286b4f
EventConnector <moritz@eventconnector.ch>
2026-01-19 11:10:53 +0100

    Harden nginx fastcgi config and restart services

---
+++ app/app/Models/Setting.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+    public $incrementing = false;
+    protected $primaryKey = 'key';
+    protected $keyType = 'string';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'key',
+        'value',
+    ];
+}
